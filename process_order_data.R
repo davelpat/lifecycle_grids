4f618683cb7e74f2f356f796196b1dbd639f6722
davelpat <davelpat@gmail.com>
2015-08-22 01:14:52 -0700

    Add recency and frequence metrics

--- process_order_data.R
+++ process_order_data.R
@@ -3,3 +3,10 @@ today <- as.Date('2012-04-11', format='%Y-%m-%d')
 
 # processing data
 orders <- dcast(orders, orderId + clientId + gender + orderdate ~ product, value.var='product', fun.aggregate=length)
+
+orders <- orders %>%
+  group_by(clientId) %>%
+  mutate(frequency=n(),
+         recency=as.numeric(today-orderdate)) %>%
+  filter(orderdate==max(orderdate)) %>%
+  filter(orderId==max(orderId))
