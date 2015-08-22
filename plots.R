05bebe8709474ea6c5322000ccaa4a4e0291bfcd
davelpat <davelpat@gmail.com>
2015-08-22 12:44:24 -0700

    fixed typo

--- plots.R
+++ plots.R
@@ -3,9 +3,9 @@ ggplot(orders, aes(x=frequency)) +
   theme_bw() +
   scale_x_continuous(breaks=c(1:10)) +
   geom_bar(alpha=0.6, binwidth=1) +
-  ggtitle("Dustribution by frequency")
+  ggtitle("Distribution by frequency")
 
 ggplot(orders, aes(x=recency)) +
   theme_bw() +
   geom_bar(alpha=0.6, binwidth=1) +
-  ggtitle("Dustribution by recency")
+  ggtitle("Distribution by recency")
